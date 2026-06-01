b071941831a2e9eb7368bc853004fe9afd0c5984
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-06-01 04:27:32 +0200

    Cree mi proyecto, Api de bienvenida

---
+++ PracticaApis/Controllers/HolaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace PracticaApis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HolaController : ControllerBase
+    {
+        [HttpGet("saludo")]
+        public IActionResult Saludo(string nombre)
+        {
+            return Ok(new
+            {
+                mensaje = $"Bienvenido {nombre}"
+            });
+        }
+    }
+}
