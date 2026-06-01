6c0c0aff5aa285dc66d7b3f628152a45d83baef0
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-06-01 22:20:33 +0200

    Creación de mi api números
    Esta es para determinar si un número entero e indica si es par o
    impar, si es primo y si es positivo, negativo o cero

---
+++ PracticaApis/Controllers/NumerosController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace PracticaApis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NumerosController : ControllerBase
+    {
+        [HttpGet("analizar")]
+        public IActionResult Analizar(int n)
+        {
+            // Par o impar
+            bool esPar = n % 2 == 0;
+
+            // Positivo, negativo o cero
+            string tipo;
+
+            if (n > 0)
+            {
+                tipo = "Positivo";
+            }
+            else if (n < 0)
+            {
+                tipo = "Negativo";
+            }
+            else
+            {
+                tipo = "Cero";
+            }
+
+            // Primo
+            bool esPrimo = true;
+
+            if (n <= 1)
+            {
+                esPrimo = false;
+            }
+            else
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+            }
+
+            return Ok(new
+            {
+                numero = n,
+                esPar = esPar,
+                esPrimo = esPrimo,
+                tipo = tipo
+            });
+        }
+    }
+}
+
