06ef6c0c3aaa13eb8a709264434f82acd5b345ba
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-06-01 23:06:22 +0200

    Endpoint contar/ ControllerTexto
    En este endpoint se esta calculando la cantidad de letras, palabras y vocales de un texto.

---
+++ PracticaApis/Controllers/TextoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaApis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TextoController : ControllerBase
+    {
+        [HttpGet("contar")]
+        public IActionResult Contar(string texto)
+        {
+            int caracteres = texto.Length;
+            int palabras = texto.Split(' ').Length;
+
+            texto = texto.ToLower();
+
+            int vocales = 0;
+
+            foreach (char letra in texto)
+            {
+                if (letra == 'a' ||
+                    letra == 'e' ||
+                    letra == 'i' ||
+                    letra == 'o' ||
+                    letra == 'u')
+                {
+                    vocales++;
+                }
+            }
+
+            return Ok(new
+            {
+                texto = texto,
+                caracteres = caracteres,
+                palabras = palabras,
+                vocales = vocales
+            });
+        }
+    }
+}
