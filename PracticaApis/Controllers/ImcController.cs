d276a57abe92d5c09c7b5c891e07f80e649fb3de
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-06-04 19:33:21 +0200

    API IMC
    Esta Api calcula el índice de masa corporal al nosotros ingresar el peso y la altura, además basado en ello; determina si la persona está en peso normal, sobre peso o peso bajo.

---
+++ PracticaApis/Controllers/ImcController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaApis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImcController : ControllerBase
+    {
+        [HttpGet("calcular")]
+        public IActionResult Calcular(double peso, double altura)
+        {
+            double imc = peso / (altura * altura);
+
+            string categoria;
+
+            if (imc < 18.5)
+            {
+                categoria = "Bajo peso";
+            }
+            else if (imc < 25)
+            {
+                categoria = "Normal";
+            }
+            else if (imc < 30)
+            {
+                categoria = "Sobrepeso";
+            }
+            else
+            {
+                categoria = "Obesidad";
+            }
+
+            return Ok(new
+            {
+                peso = peso,
+                altura = altura,
+                imc = Math.Round(imc, 2),
+                categoria = categoria
+            });
+        }
+    }
+}
