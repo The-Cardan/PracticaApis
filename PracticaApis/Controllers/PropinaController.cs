e5476c9c2775cf2f1bdef8d03453208481865f74
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-06-04 23:24:10 +0200

    API Porcentaje
    Esta api está diseñada para al uno poner el monto original de una cuenta y el porcentaje que se daría de propina, este retornaría el monto total de la cuenta.

---
+++ PracticaApis/Controllers/PropinaController.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaApis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PropinaController : ControllerBase
+    {
+        [HttpGet("calcular")]
+        public IActionResult Calcular(double monto, double porcentaje)
+        {
+            double propina = monto * porcentaje / 100;
+
+            double totalPagar = monto + propina;
+
+            return Ok(new
+            {
+                montoOriginal = monto,
+                porcentajePropina = porcentaje,
+                propina = Math.Round(propina, 2),
+                totalPagar = Math.Round(totalPagar, 2)
+            });
+        }
+    }
+}
