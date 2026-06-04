d323bf7beba2aa5b280022fea84519fc253ee1d4
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-06-04 19:50:08 +0200

    API Fechas 1.1
    Este endpoint de la Api devuelve la diferencia de días de desde una feche determinada hasta otra.

---
+++ PracticaApis/Controllers/FechasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaApis.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FechasController : ControllerBase
+    {
+        [HttpGet("diferencia")]
+        public IActionResult Diferencia(DateTime desde, DateTime hasta)
+        {
+            TimeSpan diferencia = hasta - desde;
+
+            return Ok(new
+            {
+                desde = desde.ToString("yyyy-MM-dd"),
+                hasta = hasta.ToString("yyyy-MM-dd"),
+                diasDiferencia = (int)diferencia.TotalDays
+            });
+        }
+    }
+}
