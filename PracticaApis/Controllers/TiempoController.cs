c6618b9d273ff12a816a601dc9a91c8e79449ca6
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-06-04 19:41:56 +0200

    API Tiempo
    Esta api toma el tiempo en segundos y lo lleva al formato HH/MM/SS.

---
+++ PracticaApis/Controllers/TiempoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica1WebServices.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TiempoController : ControllerBase
+    {
+        [HttpGet("formatear")]
+        public IActionResult Formatear(int segundos)
+        {
+            int horas = segundos / 3600;
+
+            int resto = segundos % 3600;
+
+            int minutos = resto / 60;
+
+            int segundosRestantes = resto % 60;
+
+            string formato = $"{horas:D2}:{minutos:D2}:{segundosRestantes:D2}";
+
+            return Ok(new
+            {
+                segundosOriginales = segundos,
+                horas = horas,
+                minutos = minutos,
+                segundos = segundosRestantes,
+                formato = formato
+            });
+        }
+    }
+}
